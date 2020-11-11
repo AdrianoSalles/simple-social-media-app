365683315f5bf41b87bf11db6ee13f93cca5406c
Adriano Salles <adrianorsalles@hotmail.com>
2020-11-11 10:25:32 -0300

    refactor: clean posts resolver

--- backend/graphql/resolvers/comments.js
+++ backend/graphql/resolvers/comments.js
@@ -3,28 +3,31 @@ const { UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth');
 
+// Create comment method
+const createComment = async (_, { postId, body }, context) => {
+  const { username } = checkAuth(context);
+
+  if (body.trim() === '') {
+    const errors = { body: 'Comment body must not be empty' };
+    throw new UserInputError('Empty comment', { errors });
+  }
+
+  const post = await Post.findById(postId);
+  const createdAt = new Date().toISOString();
+
+  if (post) {
+    const comment = { body, username, createdAt };
+    post.comments.unshift(comment);
+    await post.save();
+
+    return post;
+  } else {
+    throw new UserInputError('Post not found');
+  }
+};
+
 module.exports = {
   Mutation: {
-    createComment: async (_, { postId, body }, context) => {
-      const { username } = checkAuth(context);
-
-      if (body.trim() === '') {
-        const errors = { body: 'Comment body must not be empty' };
-        throw new UserInputError('Empty comment', { errors });
-      }
-
-      const post = await Post.findById(postId);
-      const createdAt = new Date().toISOString();
-
-      if (post) {
-        const comment = { body, username, createdAt };
-        post.comments.unshift(comment);
-        await post.save();
-
-        return post;
-      } else {
-        throw new UserInputError('Post not found');
-      }
-    },
+    createComment,
   },
 };
--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -3,58 +3,70 @@ const { AuthenticationError } = require('apollo-server');
 const checkAuth = require('../../util/checkAuth');
 const Post = require('../../models/Post');
 
+// Get all posts
+const getPosts = async () => {
+  try {
+    return await Post.find().sort({ createdAt: -1 });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+// Get post by id
+const getPost = async (_, { postId }) => {
+  try {
+    const post = await Post.findById(postId);
+
+    if (post) {
+      return post;
+    } else {
+      throw new Error('Post not found');
+    }
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+// Create a post method
+const createPost = async (_, { body }, context) => {
+  const user = checkAuth(context);
+
+  const createdAt = new Date().toUTCString();
+  const newPost = new Post({
+    body,
+    user: user.id,
+    username: user.username,
+    createdAt,
+  });
+
+  return await newPost.save();
+};
+
+// Delete a post
+const deletePost = async (_, { postId }, context) => {
+  const user = checkAuth(context);
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (user.username === post.username) {
+      await post.delete();
+      return 'Post deleted successfully';
+    }
+
+    throw new AuthenticationError('Action not allowed');
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 module.exports = {
   Query: {
-    getPosts: async () => {
-      try {
-        return await Post.find().sort({ createdAt: -1 });
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-    getPost: async (_, { postId }) => {
-      try {
-        const post = await Post.findById(postId);
-
-        if (post) {
-          return post;
-        } else {
-          throw new Error('Post not found');
-        }
-      } catch (e) {
-        throw new Error(e);
-      }
-    },
+    getPosts,
+    getPost,
   },
   Mutation: {
-    createPost: async (_, { body }, context) => {
-      const user = checkAuth(context);
-
-      const createdAt = new Date().toUTCString();
-      const newPost = new Post({
-        body,
-        user: user.id,
-        username: user.username,
-        createdAt,
-      });
-
-      return await newPost.save();
-    },
-    deletePost: async (_, { postId }, context) => {
-      const user = checkAuth(context);
-
-      try {
-        const post = await Post.findById(postId);
-
-        if (user.username === post.username) {
-          await post.delete();
-          return 'Post deleted successfully';
-        }
-
-        throw new AuthenticationError('Action not allowed');
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
+    createPost,
+    deletePost,
   },
 };
